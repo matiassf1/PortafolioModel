8ee21b63b875feab52186689413731f4220751f8
matiassf1 <sfermatias@gmail.com>
2022-03-31 00:18:44 -0300

    Actualizacion

---
+++ Cod1/scripts/app.js
@@ -0,0 +1,17 @@
+// DOM
+const enproceso1 = document.querySelector('.en_proceso1');
+const enproceso2 = document.querySelector('.en_proceso2');
+const enproceso3 = document.querySelector('.en_proceso3');
+
+
+enproceso1.addEventListener('click', () => {
+  window.alert(`La pagina Administrador de tareas aun esta en desarrollo`)
+});
+
+enproceso.addEventListener('click', () => {
+    window.alert('La pagina de Hosting esta en desarrollo.')
+  });
+  
+enproceso.addEventListener('click', () => {
+    window.alert('La pagina aun esta en la planificacion.')
+  });
